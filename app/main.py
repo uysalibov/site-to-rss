31cd3cc378a9a0e2cd4d16c8dbafd18a30663c7e
İbrahim Uysal <74019847+uysalibov@users.noreply.github.com>
2024-01-14 00:59:21 +0300

    add staticfile and template mount

--- app/main.py
+++ app/main.py
@@ -1,13 +1,18 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
 import uvicorn
 
 app = FastAPI(debug=True)
+app.mount("/static", StaticFiles(directory="./app/static"), name="static")
+
+templates = Jinja2Templates(directory="./app/templates")
 
 
 @app.get("/")
-async def homepage():
-    return {"msg": "hello kagi!"}
+async def homepage(request: Request):
+    return templates.TemplateResponse(request=request, name="index.html", context={})
 
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", reload=True)
+    uvicorn.run("main:app", port=8001, reload=True)
