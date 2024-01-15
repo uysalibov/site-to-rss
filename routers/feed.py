6824a9f70d5e2a0770f8614b31a1b1e9ca4c224a
İbrahim Uysal <74019847+uysalibov@users.noreply.github.com>
2024-01-16 02:19:07 +0300

    add target site html scraper

--- routers/feed.py
+++ routers/feed.py
@@ -1,21 +1,57 @@
 from fastapi import APIRouter, Request, Form
-from fastapi.responses import JSONResponse, RedirectResponse
-import requests
+from fastapi.responses import JSONResponse, HTMLResponse
+import requests, json
 from selectolax.parser import HTMLParser
-
-from models.feed import Feed
 
 router = APIRouter()
 
 
+@router.get("/fetch")
+async def fetch(request: Request, url: str):
+    res = requests.get(
+        url, timeout=30, headers={"user-agent": request.headers.get("user-agent")}
+    )
+    print(res.status_code)
+    return HTMLResponse(res.text.replace("<head>", f"<head>\n<base href='{url}' > "))
+
+
 @router.get("/feed")
-async def feed_get(request: Request, url: str):
-    return {"asdad": "adasd"}
+async def feed_get(request: Request):
+    return request.query_params
 
 
 @router.post("/feed")
-async def feed_post(request: Request, url: str = Form()):
-    return RedirectResponse("iframe")
+async def feed_post(request: Request):
+    form_data = await request.form()
+    print(form_data)
+    print(form_data.get("date"))
+    html = await fetch(request=request, url=form_data.get("site"))
+    print("asdad")
+    tree = HTMLParser(html.body)
+    items = [item for item in tree.css(form_data.get("element"))]
+    print("aq")
+    return [
+        {
+            "title": "".join(
+                [tit.text(strip=True) for tit in item.css(form_data.get("title"))]
+            ),
+            "description": "".join(
+                [
+                    desc.text(strip=True)
+                    for desc in item.css(form_data.get("description"))
+                ]
+            ),
+            "date": "".join(
+                [date.text(strip=True) for date in item.css(form_data.get("date"))]
+            )
+            if form_data.get("date")
+            else "",
+            "url": item.css_first(form_data.get("url")).attributes.get("href")
+            if form_data.get("url")
+            else "",
+        }
+        for item in items
+    ]
 
 
 @router.post("/auto")
