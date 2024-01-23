e3667b9265b2a05328e8d6a95e1a097de97764a5
İbrahim Uysal <74019847+uysalibov@users.noreply.github.com>
2024-01-24 00:23:41 +0300

    added fetch all feeds from db

--- internal/get_feed.py
+++ internal/get_feed.py
@@ -7,3 +7,11 @@ def get_feed(cursor: sqlite3.Cursor, _id: int):
     cursor.execute(select_query, (_id,))
 
     return cursor.fetchone()
+
+
+def get_all_feeds(cursor: sqlite3.Cursor):
+    select_query = "SELECT * FROM rss"
+
+    cursor.execute(select_query)
+
+    return cursor.fetchall()
